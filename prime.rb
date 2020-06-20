d2aa5d9cc5cb750d980b12a800c3417e78cb3410
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-20 19:29:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,8 +5,8 @@
 	if number < 2
 		false
 	else
-    (2..number-1).to_a.none? do |num|
-    num % possible_factor != 0
+    (2..number-1).to_a.all? do |factor|
+      number % factor != 0
  
     end  
   end
